c9b9f32a86c400c1fa47a56e767d4c36de72cabe
StefanoSanfilippo <64667244+StefanoSanfilippo@users.noreply.github.com>
2025-07-24 09:03:38 +0200

    Add files via upload

--- BasesdeDatos/ODS12/residus.R
+++ BasesdeDatos/ODS12/residus.R
@@ -1,1 +1,24 @@
+#=======================================
+# DADES RECOLLIDA SELECTIVA CATALUNYA
+#=======================================
 
+#=======================================
+# FONT:Recollida selectiva per habitant
+#=======================================
+
+# https://analisi.transparenciacatalunya.cat/Medi-Ambient/Estad-stiques-de-residus-municipals/69zu-w48s/about_data
+
+library(dplyr)
+library(stringr)
+library(httr)
+library(XML)
+library(ODataQuery)
+
+# URL DESCARREGA DADES FORMAT ODATA
+url <- 'https://analisi.transparenciacatalunya.cat/api/odata/v4/69zu-w48s'
+
+res <- retrieve_all(url) # Obtenir data mitjançant ODataQuery
+
+residus23 <- res %>% filter(any== "2023")
+
+writexl::write_xlsx(residus23,"BasesdeDatos/ODS12/residus23.xlsx")
