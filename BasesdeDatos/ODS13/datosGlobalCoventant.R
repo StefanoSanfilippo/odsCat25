6b45728c9f0392d8a50f45cbe42de2b8989dfdf9
StefanoSanfilippo <64667244+StefanoSanfilippo@users.noreply.github.com>
2025-07-24 09:14:55 +0200

    Add files via upload

--- BasesdeDatos/ODS13/datosGlobalCoventant.R
+++ BasesdeDatos/ODS13/datosGlobalCoventant.R
@@ -1,1 +1,21 @@
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(httr)
 
+
+# Bases de datos de proyectos y firmantes de la UE
+# https://data.jrc.ec.europa.eu/dataset/b425918f-53a1-495c-8619-cd370c302eb0
+
+url <- 'https://jeodpp.jrc.ec.europa.eu/ftp/public/JRC-OpenData/GCOM-MyCovenant/2023_Fourth_release/df1_Signatories_4th%20Release%20-%20March%202023.xlsx'
+
+GET(url, write_disk(tf <- tempfile(fileext = ".xlsx")))
+
+dfGC <- readxl::read_xlsx(tf, sheet = 3) %>% 
+    filter(coordinator_name %in% c("Province of Barcelona", 
+                                   "Province of Girona", 
+                                   "Diputació de Lleida",
+                                   "Diputació de Tarragona")) %>%  
+    select(1,2,3,5,7,8)
+
+writexl::write_xlsx(dfGC, "BasesdeDatos/ODS13/datosGlobalCoventant.xlsx")
---
+++ BasesdeDatos/ODS13/despMediAmbient-2021.R
@@ -0,0 +1,61 @@
+#=====================================
+# Política de despesa 17. Medi ambient 2021
+# ====================================
+
+library(dplyr)
+library(stringr)
+library(stringi)
+
+# Lectura de la tabla "tb_economica_cons.txt" extraída de la bbbdd Acces Liquidaciones 2022
+tb_funcional_cons <-read.delim("./BasesdeDatos/ODS1/Liquidaciones2021/tb_funcional_cons.txt",
+                               sep = ";", dec = ",", header = FALSE)
+tb_inventario <-read.delim("./BasesdeDatos/ODS1/Liquidaciones2021/tb_inventario.txt",
+                           sep = ";", dec = ",", header = FALSE)
+
+    
+
+
+# Esta tabla contiene los importes liquidados del presupuesto por id de entidad, I/G y codigo cuenta.
+
+# Explicacion de las columnas de tb_economica_cons:
+
+#V1= Id: Identificador único del Ente dependiente.
+#V2=Cdcta: Código de la cuenta económica.
+#V3=Cdfgr: Código de la cuenta funcional o por programas.
+#V4=Importe: Importe del dato económico.
+
+
+
+tb_funcional_cons <- tb_funcional_cons |> 
+    mutate(V3=trimws(V3),
+           V2=trimws(V2)) |> # Se eliminan espacios en blanco de la columna V3 (tipo de gasto)
+    filter(V3== 17) |>  # Se filtra la Política 17 Medio Ambient
+    rename_with( ~ c('Id','Cdcta','Cdfgr','Importe'),
+                 all_of(c('V1','V2','V3','V4')))
+
+
+# Explicacion de las columnas de tb_inventarios:
+
+# V1= Id: identificativo ente
+# V2= Codbdgel: codigo desprictivo entidad => Ayuntamiento = AA
+# V3= Nombreppal: nombre
+# V4= Idente
+# V5= Codente
+# V6= Mombrente
+# V7= Nsec
+# V8= Poblacion
+# V9= Estado
+
+
+
+tb_inventario <- tb_inventario |> 
+    rename_with(~ c('Id','Codbdgel','Nombreppal','Idente','Codente','Mombrente','Nsec','Poblacion','Estado'),
+                all_of(c('V1','V2','V3','V4','V5','V6','V7','V8','V9'))) |> 
+    mutate(TipoAdm= str_sub(Codbdgel, 6,7)) |>  # Extrae los codigo que classifican las administraciones Ayuntamiento=AA
+    mutate(ine = str_sub(Codbdgel, 1,5))
+
+desp17 <- tb_funcional_cons |> 
+    inner_join(tb_inventario, by= c('Id'='Idente')) |> 
+    mutate(anyo= '2021')
+
+writexl::write_xlsx(desp17, 'BasesdeDatos/ODS13/desp17MediAmb-2021.xlsx')
